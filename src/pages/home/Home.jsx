03a4117ad2640d1a5a25b520cf8141aea9069b79
kprashant544 <kasulaprashant544@gmail.com>
2024-02-10 22:52:01 +0545

    pages updated

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,7 +7,27 @@ function Home() {
           <p>Lorem Ipsum</p>
         </div>
 
-        <div className="mid-container"></div>
+        <div className="mid-container">
+          <h3>Welcome to Our Website</h3>
+          <div className="description">
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Praesentium exercitationem cumque odio, dolorem libero non
+              eligendi, amet laborum dolore, optio suscipit itaque! Molestias
+              ipsam cum dicta ut? Nesciunt, perspiciatis ullam? Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Vero sunt nobis
+              suscipit? Quasi minus quisquam incidunt? Nihil voluptatum quasi
+              voluptas minus
+            </p>
+          </div>
+          <button className="btn"> Readmore</button>
+
+          <div className="sections">
+            <div className="sec-1">jfksldfj</div>
+            <div className="sec-2">gsdggg</div>
+            <div className="sec-3">bbbefwef</div>
+          </div>
+        </div>
       </div>
     </>
   );
